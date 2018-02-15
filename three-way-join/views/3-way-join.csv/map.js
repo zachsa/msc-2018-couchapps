c4c1a3bc0dbbfc5670c3b18da5a98e3bac94f693
Zach Smith <zachsmith.sa@gmail.com>
2018-02-15 10:16:09 +0200

    initial commit

---
+++ three-way-join/views/3-way-join.csv/map.js
@@ -0,0 +1,187 @@
+/**
+ * A simplified output of the map tasks for each document type
+ * 
+ * 1) Grades
+ *    => [doc.anonIDnew, doc.Course, doc.RegAcadYear] : [doc.Percent]
+ * 
+ * 2) Demographics
+ *    => [doc.anonIDnew, 0, 0] : [
+ *         Gr12 Eng %,
+ *         Gr12 Sci %,
+ *         Gr12 Mth %,
+ *         Gr12 Mth Lit %,
+ *         Gr12 Mth Adv %,
+ *         NBT AL %,
+ *         NBT QL %,
+ *         NBT Mth %
+ *    ]
+ * 
+ * 3) Entities
+ *    => [doc.uct_id, 0, year] : [S1 Event, S2 Event]
+ *    // S1 and S2 are effectively binary fields, either 1 or 0
+ * 
+ * @param {Object} doc 
+ */
+function(doc) {
+    /* Compound key */
+    var id;
+    var course;
+    var year;
+
+    /* Decision tree */
+    var type = doc.type_;
+    switch (type) {
+        case 'grade':
+            /* Key */
+            id = doc.anonIDnew;
+            course = doc.Course;
+            year = doc.RegAcadYear;
+
+            /* Value */
+            var percent = doc.Percent || '';
+
+            /* Normalize the percent field to a number */
+            if (typeof percent !== 'number') {
+                if (typeof percent !== 'string') return;
+                var percentCleaned = percent.toUpperCase().trim();
+
+                /* Return if percent is a disallowed symbol */
+                if ([
+                        'ATT',
+                        'DE',
+                        'GIP',
+                        'LOA',
+                        'OS',
+                        'OSS',
+                        'PA',
+                        'SAT',
+                        'UNS',
+                        'AB',
+                        ''
+                    ].indexOf(percentCleaned) >= 0) return;
+
+                /* Get the percentage */
+                switch (percentCleaned) {
+                    case 'F':
+                        percent = 40;
+                        break;
+                    case 'DPR':
+                        percent = 30;
+                        break;
+                    case 'INC':
+                        percent = 30;
+                        break;
+                    case 'UF':
+                        percent = 49;
+                        break;
+                    case 'UP':
+                        percent = 45;
+                        break;
+                    default:
+                        /* Convert percent to number */
+                        percent = parseFloat(percent.substring(0, percent.length - 1));
+                        if (isNaN(percent)) return;
+                        break;
+                };
+            };
+
+            /* Output */
+            emit([id, course, year], percent);
+            break;
+
+        case 'benchmark':
+            /* Key */
+            id = doc.anonIDnew;
+            course = 0;
+            year = 0;
+
+            /* Value (each index corresponds to a benchmark) */
+            var benchmarks = [0, 0, 0, 0, 0, 0, 0, 0];
+
+            /* Normalize symbols to numbers */
+            var fuDictionary = {
+                "A*": 90,
+                "A": 80,
+                "B": 70,
+                "C": 60,
+                "D": 55,
+                "E": 50
+            };
+
+            function getDemographicGrade(gr) {
+                var g = parseInt(gr);
+                var retG;
+                if (isNaN(g)) { // True for symbols, false for numbers
+                    if (typeof gr !== 'string') return 0;
+                    retG = fuDictionary[gr.toUpperCase().trim()] || 0;
+                } else {
+                    retG = g;
+                };
+                return retG;
+            };
+
+            // Gr12 Eng
+            var gEng12 = getDemographicGrade(doc["Eng Grd12 Fin Rslt"] || "");
+            benchmarks[0] = gEng12;
+
+            // Gr12 Sci
+            var gSci12 = getDemographicGrade(doc["Phy Sci Grd12 Fin Rslt"] || "");
+            benchmarks[1] = gSci12;
+
+            // Gr12 Mth
+            var gMth12 = getDemographicGrade(doc["Math Grd12 Fin Rslt"] || "");
+            benchmarks[2] = gMth12;
+
+            // Gr12 Mth Lit
+            var gMthLit12 = getDemographicGrade(doc["Mth Lit Grd12 Fin Rslt"] || "");
+            benchmarks[3] = gMthLit12;
+
+            // Gr12 Mth Adv
+            var gMthAdv12 = getDemographicGrade(doc["Adv Mth Grd12 Fin Rslt"] || "");
+            benchmarks[4] = gMthAdv12;
+
+            // NBT AL
+            var gNbtAl = getDemographicGrade(doc["NBT AL Score"] || "");
+            benchmarks[5] = gNbtAl;
+
+            // NBT QL
+            var gNbtQl = getDemographicGrade(doc["NBT QL Score"] || "");
+            benchmarks[6] = gNbtQl;
+
+            // NBT Mth
+            var gNbtMth = getDemographicGrade(doc["NBT Math Score"] || "");
+            benchmarks[7] = gNbtMth;
+
+            /* Output */
+            emit([id, course, year], benchmarks);
+            break;
+
+        case 'event':
+            var date = new Date(doc.event_date);
+            var epoch = date.getTime();
+
+            /* Key */
+            id = doc.uct_id;
+            course = 0;
+            year = date.getFullYear();
+
+            /* Value */
+            var event = [0, 0];
+
+            /* Midpoint =  Sunday, July 17, 2016 10:00:00 PM */
+            var midDate = 1468792800000
+            var semester = (epoch - midDate >= 0) ? 2 : 1;
+            if (semester === 1) {
+                event[0] = 1;
+            } else {
+                event[1] = 1;
+            };
+
+            /* Output */
+            emit([id, course, year], event);
+            break;
+
+        default:
+            break;
+    };
+};
---
+++ variance/views/variance.csv/map.js
@@ -0,0 +1,142 @@
+/**
+ * Demographics are emitted in the following form:
+ *   key: doc.anonIDnew
+ *   value: [
+ *         Gr12 Eng %, // i = 0
+ *         Gr12 Sci %, // i = 1
+ *         Gr12 Mth %, // i = 2
+ *         NBT AL %, // i = 3
+ *         NBT QL %, // i = 4
+ *         NBT Mth %, // i = 5
+ *         Avg Gr12 %, // i = 6
+ *         Avg Gr12 % (Dbl Mth), // i = 7
+ *         Avg Gr12 % (Dbl Mth & Sci), // i = 8
+ *         Avg NBT %, // i = 9
+ *         Avg NBT % (Dbl AL), // i = 10
+ *         Avg NBT % (Dbl QL), // i = 11
+ *         Avg NBT % (Dbl Mth), // i = 12
+ *         Avg NBT % (Dbl AL/QL), // i = 13
+ *         Avg NBT % (Dbl AL/Mth), // i = 14
+ *         Avg NBT % (Dbl QL/Mth), // i = 15
+ *         AVG Gr12 & NBT, // i = 16
+ *         AVG Gr12 & NBT (Dbl Gr12 Mth), // i = 17
+ *         AVG Gr12 & NBT (Dbl Gr12 Mth & Sci) // i = 18
+ *   ]
+ * 
+ * @param {Object} doc 
+ */
+function(doc) {
+    var type = doc.type_ || null;
+    if (!type === 'benchmark') return;
+
+    /* Load Decimal library */
+    var Decimal = require("views/lib/decimal");
+
+    /**
+     * Reduce array to avg (all elements must be numbers)
+     * @param {boolean} ignoreZero 
+     */
+    Array.prototype.avg = function(ignoreZero) {
+        ignoreZero = ignoreZero || false;
+        var zeros = 0;
+        var sum = new Decimal(this.reduce(function(a, b) {
+            if (ignoreZero) {
+                if (b === 0) {
+                    zeros++;
+                };
+            };
+            return a + b;
+        }));
+        var divisor = new Decimal(this.length - zeros);
+        return sum.dividedBy(divisor).toFixed(2);
+    };
+
+    /* Key - the Student ID */
+    var id = doc.anonIDnew;
+
+    /* Value (each index corresponds to a benchmark) */
+    var benchmarks = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+
+    /* Normalize symbols to numbers */
+    var fuDictionary = {
+        "A*": 90,
+        "A": 80,
+        "B": 70,
+        "C": 60,
+        "D": 55,
+        "E": 50
+    };
+
+    function getDemographicGrade(gr) {
+        var g = parseFloat(gr).toFixed(2);
+        var retG;
+        if (isNaN(g)) { // True for symbols, false for numbers
+            if (typeof gr !== 'string') return 0;
+            retG = fuDictionary[gr.toUpperCase().trim()] || 0;
+        } else {
+            retG = g;
+        };
+        return retG;
+    };
+
+    // i = 0
+    var gEng12 = Number(getDemographicGrade(doc["Eng Grd12 Fin Rslt"] || ""));
+    benchmarks[0] = gEng12;
+    // i = 1
+    var gSci12 = Number(getDemographicGrade(doc["Phy Sci Grd12 Fin Rslt"] || ""));
+    benchmarks[1] = gSci12;
+    // i = 2
+    var gMth12 = Number(getDemographicGrade(doc["Math Grd12 Fin Rslt"] || ""));
+    benchmarks[2] = gMth12;
+    // i = 3
+    var gNbtAl = Number(getDemographicGrade(doc["NBT AL Score"] || ""));
+    benchmarks[3] = gNbtAl;
+    // i = 4
+    var gNbtQl = Number(getDemographicGrade(doc["NBT QL Score"] || ""));
+    benchmarks[4] = gNbtQl;
+    // i = 5
+    var gNbtMth = Number(getDemographicGrade(doc["NBT Math Score"] || ""));
+    benchmarks[5] = gNbtMth;
+    // i = 6
+    var g12Avg = Number([gEng12, gSci12, gMth12].avg(true));
+    benchmarks[6] = g12Avg;
+    // i = 7
+    var g12AvgMth = Number([gEng12, gSci12, gMth12, gMth12].avg(true));
+    benchmarks[7] = g12AvgMth;
+    // i = 8
+    var g12AvgMthSci = Number([gEng12, gSci12, gMth12, gMth12, gSci12].avg(true));
+    benchmarks[8] = g12AvgMthSci;
+    // i = 9
+    var gNbtAvg = Number([gNbtAl, gNbtQl, gNbtMth].avg(true));
+    benchmarks[9] = gNbtAvg;
+    // i = 10
+    var gNbtAvgAl = Number([gNbtAl, gNbtQl, gNbtMth, gNbtAl].avg(true));
+    benchmarks[10] = gNbtAvgAl;
+    // i = 11
+    var gNbtAvgQl = Number([gNbtAl, gNbtQl, gNbtMth, gNbtQl].avg(true));
+    benchmarks[11] = gNbtAvgQl;
+    // i = 12
+    var gNbtAvgMth = Number([gNbtAl, gNbtQl, gNbtMth, gNbtMth].avg(true));
+    benchmarks[12] = gNbtAvgMth;
+    // i = 13
+    var gNbtAvgAlQl = Number([gNbtAl, gNbtQl, gNbtMth, gNbtAl, gNbtQl].avg(true));
+    benchmarks[13] = gNbtAvgAlQl;
+    // i = 14
+    var gNbtAvgAlMth = Number([gNbtAl, gNbtQl, gNbtMth, gNbtAl, gNbtAvgMth].avg(true));
+    benchmarks[14] = gNbtAvgAlMth;
+    // i = 15
+    var gNbtAvgQlMth = Number([gNbtAl, gNbtQl, gNbtMth, gNbtQl, gNbtMth].avg(true));
+    benchmarks[15] = gNbtAvgQlMth;
+    // i = 16
+    var gGr12NbtAvg = Number([g12Avg, gNbtAvg].avg(true));
+    benchmarks[16] = gGr12NbtAvg;
+    // i = 17
+    var gGr12NbtAvgGr12Mth = Number([g12Avg, gNbtAvg, gMth12].avg(true));
+    benchmarks[17] = gGr12NbtAvgGr12Mth;
+    // i = 18
+    var gGr12NbtAvgGr12MthSci = Number([g12Avg, gNbtAvg, gMth12, gSci12].avg(true));
+    benchmarks[18] = gGr12NbtAvgGr12MthSci;
+
+    /* Output */
+    emit(id, benchmarks);
+};
